bafa1fd0cacf93e4443cb8a9f86ccd248e59386e
Nikita0728 <kunwornikita123@gmail.com>
2024-04-16 18:39:29 +0545

    hello world

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Header } from './components/Header';
 import { Headermax } from './components/Headermax';
 import { Programs } from './components/Programs';
+import {About} from './components/About';
 function App() {
   return (
     <>
    <Header/>
    <Headermax/>
    <Programs/>
+   <About/>
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import about from '../assets/edusity_assets/about.png'
+import play_icon from '../assets/edusity_assets/play-icon.png'
+
+export const About = () => {
+  return (
+    <div className='about'>
+        <div className="div-left">
+                <img src={about} alt='' className='about-img'/>
+                <img src={play_icon} alt='' className='play-icon'/>
+        </div>
+        <div className="div-right">
+            <h3>About University</h3>
+            <h2> Nurturing tomorrow's future.</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque repudiandae, veniam at corporis magnam totam doloribus, accusamus illum nesciunt voluptatem quibusdam natus qui. Atque, illo id explicabo deleniti libero ipsa rerum odit asperiores fugiat?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis amet dolore ipsam aliquid aperiam? Facere modi repellendus nostrum corporis eveniet nihil rerum repellat ratione?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque suscipit quod eligendi temporibus! Voluptatem laborum eveniet neque alias labore placeat, dolores ducimus perferendis veniam expedita nostrum reiciendis. Earum, suscipit nobis.</p>
+
+        </div>
+
+    </div>
+  )
+}
