672a43c09e7d5e37dfa10b8338f100d1a39b0e11
Nikita0728 <kunwornikita123@gmail.com>
2024-04-17 00:10:28 +0545

    final commit of the day

--- src/components/Campus.jsx
+++ src/components/Campus.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import './Styles.css'
+import g_1 from '../assets/edusity_assets/gallery-1.png'
+import g_2 from '../assets/edusity_assets/gallery-2.png'
+import g_3 from '../assets/edusity_assets/gallery-3.png'
+import g_4 from '../assets/edusity_assets/gallery-4.png' 
 
 export const Campus = () => {
   return (
+        <div className=" cphotos">
     <div>
-        <div className="div-right cphotos">
+        <div className='new div-right'  >
      <h3>Gallery</h3>
      <div><h2> Campus Photos</h2></div>
      </div>
-    </div>
+     <div className='campus'>
+       <div className='gallery'>  
+        <img src={g_1} alt=''></img>
+        <img src={g_2} alt=''/>
+
+        <img src={g_3} alt=''/>
+        <img src={g_4} alt=''/>
+
+     
+        </div>
+            </div>
+            </div>
+     </div>
   )
 }
